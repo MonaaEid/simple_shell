32ade2510563d444d009bd3b7123f14eb9b3505b
root <root@DESKTOP-9DECJ33.localdomain>
2023-08-10 01:27:10 +0300

    Add shell.h

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/*Include libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define _GNU_SOURCE
+#define BUFFER_SIZE 1024
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+
+#endif
