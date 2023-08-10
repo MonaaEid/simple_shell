6638946f5691c825704adebbbd473beb1004890f
MonaaEid <monaa_eid@hotmail.com>
2023-08-10 20:53:20 +0300

    update shell.h

--- shell.h
+++ shell.h
@@ -5,9 +5,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <limits.h>
 
 #define _GNU_SOURCE
 #define BUFFER_SIZE 1024
 
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+
+int _putchar(int currChar);
+int print_string(char *currString);
+int _printf(const char *format, ...);
+int _strlen(char *s);
 #endif
