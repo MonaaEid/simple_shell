0e9fbf564d0cdc8ea13dca07134d9158f031af08
MonaaEid <monaa_eid@hotmail.com>
2023-08-13 12:09:24 +0300

    split_line.c file

---
+++ split_line.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * split_line - function that split a line into tokens
+ * @line: the line will be splitted
+ * Return: the array of tokens
+*/
+char **split_line(char *line)
+{
+	int bufsize = BUFFER_SIZE;
+	int position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (tokens == NULL)
+	{
+		fprintf(stderr, "Error: memory allocation failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, DELIM);
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+		if (position >= bufsize)
+		{
+			bufsize += BUFFER_SIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (tokens == NULL)
+			{
+				fprintf(stderr, "Error: memory allocation failed.\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, DELIM);
+	}
+	tokens[position] = NULL;
+	return (tokens);
+}
