b6f231762b4c8a6f0107c42240475f7faaf822c0
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-08-19 12:43:54 +0300

    Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * execute_command - function that execute a command
  * @args: the arguments will be executed
@@ -13,7 +14,12 @@ int execute_command(char **args)
 	{
 		return (1);
 	}
-
+	if (_strcmp(args[0], "exit") == 0)
+		return (0);
+	if (_strcmp(args[0], "env") == 0)
+		return (env_builtin());
+	if (_strcmp(args[0], "cd") == 0)
+		status = cd_builtin(args);
 	pid = fork();
 	if (pid == -1)
 	{
@@ -23,7 +29,8 @@ int execute_command(char **args)
 
 	if (pid == 0)
 	{
-		if (execvp(args[0], args) == -1)
+		/*if (execvp(args[0], args) == -1)*/
+		if (execve(args[0], args, NULL) == -1)
 		{
 			perror(args[0]);
 		}
@@ -37,4 +44,18 @@ int execute_command(char **args)
 			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 			return (WEXITSTATUS(status));
 	}
+}
+/**
+ * env_builtin - prints the current environment
+ * Return: 1 if success, -1 if error
+ */
+int env_builtin(void)
+{
+    int i = 0;
+    while (environ[i] != NULL)
+    {
+        _printf("%s\n", environ[i]);
+        i++;
+    }
+    return (1);
 }
