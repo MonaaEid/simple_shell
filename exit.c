744d49f02221b6691fb5506188e66355f95e1690
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-08-23 01:34:37 +0300

    Update exit.c

--- exit.c
+++ exit.c
@@ -16,21 +16,3 @@ void exitShell(void)
 	printf("Exiting the shell.\n");
 	exit(0);
 }
-int main(void)
-{
-	char input[CmLength];
-
-	while (1)
-	{
-		printf("$ ");
-		fgets(input, sizeof(input), stdin);
-		input[strcspn(input, "\n")] = '\0';
-		if (strcmp(input, "exit") == 0)
-		{
-			exitShell(void);
-		} else
-			system(input);
-
-	}
-	return (0);
-}
