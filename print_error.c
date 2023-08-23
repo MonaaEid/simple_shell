42a3d3a1277fb3cc1e2d988189c6520d1d902460
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-08-23 03:35:55 +0300

    Add files via upload

---
+++ print_error.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+void print_error(char *progname, char *arg)
+{
+	char *errmsg;
+	int i, j;
+
+	errmsg = malloc(strlen(progname) + strlen(arg) + 6);
+	if (errmsg == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	j = 0;
+	while (progname[j] != '\0')
+	{
+		errmsg[i] = progname[j];
+		i++;
+		j++;
+	}
+	errmsg[i] = ':';
+	i++;
+	errmsg[i] = ' ';
+	i++;
+	errmsg[i] = '1';
+	i++;
+	errmsg[i] = ':';
+	i++;
+	errmsg[i] = ' ';
+	i++;
+	j = 0;
+	while (arg[j] != '\0')
+	{
+		errmsg[i] = arg[j];
+		i++;
+		j++;
+	}
+	errmsg[i] = '\0';
+	perror(errmsg);
+	free(errmsg);
+}
