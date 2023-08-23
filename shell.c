1c58c9fad4542a5ea81d17489c573c4ca1d50b7f
MonaaEid <59332150+MonaaEid@users.noreply.github.com>
2023-08-23 03:44:00 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -9,7 +9,8 @@
  */
 /*int main(int ac, char **av)*/
 
-int main(void)
+/*int main(void)*/
+int main(int ac, char **av)
 {
 	/*FILE *stream;*/
 	char *line;
@@ -17,20 +18,16 @@ int main(void)
 	int status;
 
 
-	/*(void)ac; (void)av;*/
+	(void)ac; (void)av;
 
 	do {
 		write(1, "(monsh) $ ", 11);
 		/*_printf("(monsh) $ ");*/
 
 		line = read_line();
-
 		args = split_line(line);
-
-		status = execute_command(args);
-
+		status = execute_command(args, av[0]);
 		free(line);
-
 		free(args);
 
 	} while (status);
