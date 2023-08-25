028f9c84873f38f4106bec5fe539cbb8d3f2cb2d
MonaaEid <monaa_eid@hotmail.com>
2023-08-25 14:02:38 -0700

    update

--- execute_command.c
+++ execute_command.c
@@ -14,8 +14,7 @@ int execute_builtin(char **args)
 {
 	if (_strcmp(args[0], "exit") == 0)
 	{
-		free(args);
-		exit(0);
+		return (EXIT_SUCCESS);
 	}
 	if (_strcmp(args[0], "env") == 0)
 		return (env_builtin());
