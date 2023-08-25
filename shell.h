cb48bc7b3b6db627e1b342de6e89078ffede5a18
AmrEllaithy0007 <amr.ellaithy007@gmail.com>
2023-08-25 05:06:05 -0700

    deleting log

--- shell.h
+++ shell.h
@@ -66,7 +66,5 @@ char *enter(char *string);
 void print_number(unsigned int n);
 void print_number_int(int n);
 int print_echo(char **cmd);
-/*logical operator func*/
-void executeCommand(char *command);
 
 #endif
